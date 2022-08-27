20b94661df57f139a131075f9da30b0849b81f3e
Prophecyuntold <aminur999@hotmail.com>
2022-08-27 15:28:43 +0100

    add header page layout

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import SubHeading from '../../components/SubHeading/SubHeading';
 
+import { images } from '../../constants';
 import './Header.css';
 
+
+
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading  title="Chase the New Flavour"/>
+      <h1 className='app__header-h1'>The key to fine dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}>The design looks great! 
+As someone who is learning web dev something that would be really useful would be a video where we see you iterate on creating a design or reproducing it through css. If we can follow your train of thought and see the trial and error, it becomes a lot more relatable to the experience of building a website from scratch and trying to make it look good.
+That being said this is still very useful and I'm going to steal some styling ideas without a doubt : </p>
+      <button className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img'/>
+    </div>
   </div>
 );
 
